858e6239aba60e106b7f775102f140dd6aba6a26
Sathwik puttala <sathwikputtala2003@gmail.com>
2025-12-07 11:29:50 +0530

    joins changed

--- Joins/Self Join.sql
+++ Joins/Self Join.sql
@@ -1,7 +1,4 @@
--- Self Join
-SELECT 
-    t1.name AS sport1,
-    t2.name AS sport2
-FROM sports as  t1
-JOIN sports as t2
-on t1.id <> t2.id;
+create table nine(
+id int auto_increment primary key,
+text varchar("
+)
