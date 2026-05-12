b5310ff21013e47333f21aacfc5c491844f6ced0
Sathwik puttala <sathwikputtala2003@gmail.com>
2026-05-12 17:40:00 +0530

    updated introduction file

--- SQL FULL COURSE/Introduction/Introduction.sql
+++ SQL FULL COURSE/Introduction/Introduction.sql
@@ -1,2 +1,10 @@
 -- This i my new Mysql Course
 -- use full for future references 
+-- mysql is a software or system 
+-- sql is a language used for communicating with mysql
+-- types :
+-- 1.sql
+-- 2.no sql
+
+-- sql is mysql and no sql is mongo db
+-- all data is stored in the form of tables in mysql 
