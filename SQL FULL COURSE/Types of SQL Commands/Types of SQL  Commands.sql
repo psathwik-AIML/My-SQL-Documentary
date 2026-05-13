acc5d2ed75933f79951f5653656f9d157a31f665
Sathwik puttala <sathwikputtala2003@gmail.com>
2026-05-13 17:05:42 +0530

    Types of sql commands file added

---
+++ SQL FULL COURSE/Types of SQL Commands/Types of SQL  Commands.sql
@@ -0,0 +1,29 @@
+-- Types of Sql Commands 
+
+-- 1.DDL (Data Definition Language)
+
+-- COMMANDS:
+-- CREATE- create a database or table
+-- TRUNCATE- delete all data from a table
+-- DROP- delete a full table
+-- RENAME- rename a table 
+-- ALTER- modify the table structure
+
+-- 2.DML (Data Manipulation Language)
+
+-- COMMANDS:
+-- INSERT- insert a new record to table
+-- UPDATE- update a existing table 
+-- DELETE- delete a particular record from a table
+
+-- 3.DQL (Data Query Language)
+
+-- COMMANDS:
+-- SELECT- get and display data from database 
+
+-- 4.TCL (Transaction Control Language)
+
+-- COMMANDS:
+-- COMMIT- save the changes done
+-- ROLLBACK- undo the changes done
+-- SAVEPOINT- save a point to roll back
