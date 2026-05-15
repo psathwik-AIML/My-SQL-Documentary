a5180c61d31a26eb5ffec143ac6aef6dfffcd27f
Sathwik puttala <sathwikputtala2003@gmail.com>
2026-05-15 22:38:12 +0530

    Data Types

---
+++ SQL FULL COURSE/Data Types/Data Types.sql
@@ -0,0 +1,8 @@
+-- Data Types decide what kind of data to be stored in columns
+
+-- Types:
+-- 1.Int - stores whole numbers
+-- 2.Float -stores decimal values
+-- 3.Varchar - strings 
+-- 4.Boolean - true /false
+-- 5.Char - fixed string
